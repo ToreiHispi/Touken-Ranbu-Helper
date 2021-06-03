e1a57176b2814d8ebcbdcce546d3cf9de3f2c244
ToreiHispi <37494348+ToreiHispi@users.noreply.github.com>
2021-06-03 16:17:27 -0400

    added files

--- Firefox/bridge/detect.js
+++ Firefox/bridge/detect.js
@@ -4,14 +4,6 @@ chrome.runtime.sendMessage({
   message: {
     title: 'Welcome to ~TKRB Helper~',
     message: 'To open the tool, press F12 or right-click in a blank space and select "Inspect".',
-<<<<<<< HEAD
-<<<<<<< HEAD
-    context: 'Build version：2.1.2'
-=======
-    context: 'Build version：2.2.0'
->>>>>>> dev
-=======
     context: 'Build version：2.2.2'
->>>>>>> dev
   }
 })
